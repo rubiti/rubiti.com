674b927a38cf9084a754bf424cc3e581408c633d
abrandao <andersonbrandaolustosa@gmail.com>
2020-04-30 20:02:42 +0000

    admin template in progress

--- app/javascript/packs/admin/admin.js
+++ app/javascript/packs/admin/admin.js
@@ -4,6 +4,7 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import "@fortawesome/fontawesome-free/js/all";
 import "../../stylesheets/admin/admin";
 import "./admin_script";
 import 'core-js/stable';
